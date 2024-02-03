557970d800e7fc8c9aace08f6eb4e5ee3147128f
JeampierM <157296709+JeampierM@users.noreply.github.com>
2024-02-03 06:41:39 +0000

    guardando cambios

--- Problema9.py
+++
@@ -1,9 +0,0 @@
-def omitirVocales(cadena:str):
-    vocales = "aeiouAEIOU"
-    resultado = ''.join(caracter for caracter in cadena if caracter not in vocales)
-    return resultado
-
-texto = input("Ingrese una cadena de texto: ")
-
-resultado = omitirVocales(texto)
-print("Texto con vocales omitidas:", resultado)
